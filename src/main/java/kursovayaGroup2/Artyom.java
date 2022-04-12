da11bf9f2adc5f028449f645695fb01942dc5771
AirstaNs <94823184+AirstaNs@users.noreply.github.com>
2022-04-12 21:05:31 +0500

    create hello

--- src/main/java/kursovayaGroup2/Artyom.java
+++ src/main/java/kursovayaGroup2/Artyom.java
@@ -3,5 +3,7 @@ package kursovayaGroup2;
 public class Artyom {
     public static void main(String[] args) {
         System.out.println("ARTEM");
+        System.out.println("а");
+        System.out.println("б");
     }
 }
