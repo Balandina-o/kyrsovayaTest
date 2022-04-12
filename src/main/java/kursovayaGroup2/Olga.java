47a0f69b0dca5304971418503efd57fd6da26b14
Balandina-o <balandina.olga.2021@gmail.com>
2022-04-12 21:22:47 +0500

    changing class

--- src/main/java/kursovayaGroup2/Olga.java
+++ src/main/java/kursovayaGroup2/Olga.java
@@ -3,5 +3,8 @@ package kursovayaGroup2;
 public class Olga {
 	//WHY ORIGIN MASTER IS HERE?
 	
+	public static void main(String[] args) {
+        System.out.println("To be or not to be?..");
+    }
 	
 }
